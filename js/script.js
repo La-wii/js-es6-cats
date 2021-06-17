832be5ead7ec787db1fd8800d7294b2ec53f884a
La-wii <sentilavale@gmail.com>
2021-06-17 18:35:18 +0200

    add style element.colore

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@
 // Definire un array di oggetti; ogni oggetto rappresenta un gatto, che è caratterizzato da: nome, età, colore e sesso.
 
 const gattini = [
-    {nome: 'Enrico', eta: 8, colore: 'Nero', sesso: 'Maschio'},
-    {nome: 'Ciccio', eta: 12, colore: 'Bianco', sesso: 'Maschio'},
-    {nome: 'Teo', eta: 3, colore: 'Arancione', sesso: 'Maschio'},
-    {nome: 'Zora', eta: 11, colore: 'Nero', sesso: 'Femmina'},
-    {nome: 'Miciolina', eta: 1, colore: 'Grigio', sesso: 'Femmina'},
-    {nome: 'Giuditta', eta: 3, colore: 'Grigio', sesso: 'Femmina'},
+    {nome: 'Enrico', eta: 8, colore: '#000000', sesso: 'Maschio'},
+    {nome: 'Ciccio', eta: 12, colore: '#2f1e1a', sesso: 'Maschio'},
+    {nome: 'Teo', eta: 3, colore: '#ff6800', sesso: 'Maschio'},
+    {nome: 'Zora', eta: 11, colore: '#000000', sesso: 'Femmina'},
+    {nome: 'Miciolina', eta: 1, colore: '#4a4c4b', sesso: 'Femmina'},
+    {nome: 'Giuditta', eta: 3, colore: '#949494', sesso: 'Femmina'},
 ]
 
 console.log(gattini);
@@ -20,15 +20,12 @@ gattini.forEach((element) => {
     `
     <ul>
         <li>
-            Nome : ${element.nome} 
-        <br>
-            Colore: ${element.colore}
+            ${element.nome} : <i class="fas fa-cat" style="color:${element.colore}"></i>
         </li>
         <hr>
     <ul>
-    `    
-}
-);
+    `;    
+});
 
 // Milestone 2 
 // Dividere i gatti in due contenitori distinti in base al sesso e aggiungere a fianco di ogni gattino un fiocco colorato di rosa, se femmina, o di blu, se maschio. 
@@ -47,7 +44,6 @@ for (let i = 0; i < gattini.length; i++){
 }
 console.log(maschi);
 console.log(femmine);
-
 
 
 // Il colore del fiocco deve essere più tenue se il gatto è più giovane, più scuro se il gatto è più vecchio.
