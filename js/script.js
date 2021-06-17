6b5e37dded75cb0d8bb48c03aab31610abc205cb
La-wii <sentilavale@gmail.com>
2021-06-17 13:45:44 +0200

    modificati i colori + gattini.forEach()

--- js/script.js
+++ js/script.js
@@ -3,14 +3,28 @@
 // Definire un array di oggetti; ogni oggetto rappresenta un gatto, che è caratterizzato da: nome, età, colore e sesso.
 
 const gattini = [
-    {nome: 'Enrico', eta: 8, colore: '#000000', sesso: 'Maschio'},
-    {nome: 'Ciccio', eta: 12, colore: '#ffffff', sesso: 'Maschio'},
-    {nome: 'Teo', eta: 3, colore: '#847c56', sesso: 'Maschio'},
-    {nome: 'Zora', eta: 11, colore: '#000000', sesso: 'Femmina'},
-    {nome: 'Miciolina', eta: 1, colore: '#bcbbb8', sesso: 'Femmina'},
-    {nome: 'Giuditta', eta: 3, colore: '#60605d', sesso: 'Femmina'},
+    {nome: 'Enrico', eta: 8, colore: 'Nero', sesso: 'Maschio'},
+    {nome: 'Ciccio', eta: 12, colore: 'Bianco', sesso: 'Maschio'},
+    {nome: 'Teo', eta: 3, colore: 'Arancione', sesso: 'Maschio'},
+    {nome: 'Zora', eta: 11, colore: 'Nero', sesso: 'Femmina'},
+    {nome: 'Miciolina', eta: 1, colore: 'Grigio', sesso: 'Femmina'},
+    {nome: 'Giuditta', eta: 3, colore: 'Grigio', sesso: 'Femmina'},
 ]
 
 console.log(gattini);
 
 // pt 2 -Tramite il forEach(), stampare in pagina tutti i gattini, ciascuno con il proprio colore e il proprio nome.
+
+gattini.forEach((element) => {
+    document.getElementById("gatti").innerHTML +=
+    `
+    <ul>
+        <li>
+            Nome : ${element.nome} 
+        <br>
+            Colore: ${element.colore}
+        </li>
+    <ul>
+    `    
+}
+);
