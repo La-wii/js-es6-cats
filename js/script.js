25384d502d83e27bbccbfebaa1312374701ecb1d
La-wii <sentilavale@gmail.com>
2021-06-17 13:22:56 +0200

    const array oggetti gattini

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Milestone 1 
+
+// Definire un array di oggetti; ogni oggetto rappresenta un gatto, che è caratterizzato da: nome, età, colore e sesso.
+
+const gattini = [
+    {nome: 'Enrico', eta: 8, colore: '#000000', sesso: 'Maschio'},
+    {nome: 'Ciccio', eta: 12, colore: '#ffffff', sesso: 'Maschio'},
+    {nome: 'Teo', eta: 3, colore: '#847c56', sesso: 'Maschio'},
+    {nome: 'Zora', eta: 11, colore: '#000000', sesso: 'Femmina'},
+    {nome: 'Miciolina', eta: 1, colore: '#bcbbb8', sesso: 'Femmina'},
+    {nome: 'Giuditta', eta: 3, colore: '#60605d', sesso: 'Femmina'},
+]
+
+console.log(gattini);
+
+// pt 2 -Tramite il forEach(), stampare in pagina tutti i gattini, ciascuno con il proprio colore e il proprio nome.
